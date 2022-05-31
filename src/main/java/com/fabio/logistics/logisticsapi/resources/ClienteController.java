9cfb3aae14a3b942d3613390548dbc9df227b931
Fabio Matos <fabio.thunder@hotmail.com>
2022-05-30 21:01:22 -0300

    implementação das operações CRUD

--- src/main/java/com/fabio/logistics/logisticsapi/resources/ClienteController.java
+++ src/main/java/com/fabio/logistics/logisticsapi/resources/ClienteController.java
@@ -4,36 +4,58 @@ import com.fabio.logistics.logisticsapi.model.Cliente;
 import com.fabio.logistics.logisticsapi.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
+@RequestMapping("/clientes")
 @AllArgsConstructor
 @RestController
 public class ClienteController {
     @Autowired
     private ClienteRepository repository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
         return repository.findAll();
     }
 
-    @GetMapping("/clientes/{clienteId}")
+    @GetMapping("/{clienteId}")
     public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
-        Optional<Cliente> cliente = repository.findById(clienteId);
+     return repository.findById(clienteId).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
 
-        if(cliente.isPresent()) {
-            return ResponseEntity.ok(cliente.get());
-        }
-        return ResponseEntity.notFound().build();
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+        if(!repository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(clienteId);
+        cliente = repository.save(cliente);
+        return ResponseEntity.ok(cliente);
+
+    }
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> deletar(@PathVariable Long clienteId) {
+
+        if(!repository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.deleteById(clienteId);
+        return ResponseEntity.noContent().build();
+    }
 
 }
